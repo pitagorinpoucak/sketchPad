ae655a0290366c9d226766d42060421cb423d304
pitagorinpoucak <rookie.1.8@gmail.com>
2022-02-11 15:38:38 +0100

    Smooth resizing
    - Fixed resizing issue. Needed to save width in variable instead of
    calculating it every time for width and height

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,46 @@
-const gridSize = 10; //prompt("How big should the grid be?");
-let colorsArray = ["blue", "black", "yellow", "green", "red", "grey", "orange"];
+let gridSize = 16;
+let gridWidth = document.querySelector(".bar.main").clientWidth;
 
-drawGrid();
+drawGrid(gridSize);
 
-function drawGrid() {
+document.getElementById("gridSize").addEventListener("change", (e) => {
+  clearGridDivs();
+  drawGrid(e.target.valueAsNumber);
+});
+
+//drawGrid();
+
+function clearGridDivs() {
+  const grid = document.getElementById("grid-container");
+  let element = grid.lastElementChild;
+  while (element) {
+    grid.removeChild(element);
+    element = grid.lastElementChild;
+  }
+}
+
+function drawGrid(size) {
   const gridContainer = document.getElementById("grid-container");
-  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 60px)`;
+  gridContainer.removeChild;
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, ${size}fr)`;
 
-  for (let i = 0; i < gridSize * gridSize; i++) {
+  for (let i = 0; i < size * size; i++) {
     gridContainer.appendChild(document.createElement("div"));
   }
   const gridItems = document.querySelectorAll("#grid-container div");
   gridItems.forEach((item) => {
     item.classList.add("grid-item");
     item.addEventListener("mouseover", changeColor);
+    item.style.width = `${gridWidth / size}px`;
+    item.style.height = `${gridWidth / size}px`;
   });
 }
 
 function changeColor(e) {
-  const colour =
-    colorsArray[Math.floor(Math.random() * (colorsArray.length - 1))];
-  e.target.style.backgroundColor = colour;
+  const colour = () => {
+    let n = (Math.random() * 0xffffff * 1000000).toString(16);
+    return "#" + n.slice(0, 6);
+  };
+
+  e.target.style.backgroundColor = colour();
 }
