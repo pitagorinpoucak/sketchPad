1f656fd07c0cbfdb483aa59104f3e905de39e16e
pitagorinpoucak <rookie.1.8@gmail.com>
2022-02-11 19:58:05 +0100

    Darken Function
    - Added and setup a darken function to darken already colored grid items

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ const gridWidth = document.querySelector(".bar.main").clientWidth;
 let mode = "black";
 let colorPicked = false;
 let brushColor = "black";
+let darken = false;
 
 drawGrid(gridSize);
 
@@ -31,6 +32,7 @@ function drawGrid(size) {
   const gridItems = document.querySelectorAll("#grid-container div");
   gridItems.forEach((item) => {
     item.classList.add("grid-item");
+    //item.style.backgroundColor = "#ffffff";
     item.addEventListener("mouseover", changeColor);
     item.style.width = `${gridWidth / size}px`;
     item.style.height = `${gridWidth / size}px`;
@@ -48,23 +50,27 @@ function changeColor(e) {
 
     switch (mode) {
       case "black":
-        e.target.style.backgroundColor = "black";
+        e.target.style.backgroundColor = "#000000";
         break;
       case "eraser":
-        e.target.style.backgroundColor = "white";
+        e.target.style.backgroundColor = "#ffffff";
         break;
-      case "shade":
-      //TODO
       case "rainbow":
         e.target.style.backgroundColor = colour();
         break;
+      case "darken":
+        e.target.style.backgroundColor = pSBC(
+          -0.1,
+          e.target.style.backgroundColor,
+          false,
+          true
+        );
+        break;
     }
-
-    // e.target.style.backgroundColor = colour();
   }
 }
+
 function clean() {
-  console.log("click");
   const gridItems = document.querySelectorAll(".grid-item");
   gridItems.forEach((item) => (item.style.backgroundColor = "white"));
 }
@@ -80,3 +86,103 @@ function setColor(e) {
   colorPicked = true;
   brushColor = e.target.value;
 }
+
+const pSBC = (p, c0, c1, l) => {
+  let r,
+    g,
+    b,
+    P,
+    f,
+    t,
+    h,
+    i = parseInt,
+    m = Math.round,
+    a = typeof c1 == "string";
+  if (
+    typeof p != "number" ||
+    p < -1 ||
+    p > 1 ||
+    typeof c0 != "string" ||
+    (c0[0] != "r" && c0[0] != "#") ||
+    (c1 && !a)
+  )
+    return null;
+  if (!this.pSBCr)
+    this.pSBCr = (d) => {
+      let n = d.length,
+        x = {};
+      if (n > 9) {
+        ([r, g, b, a] = d = d.split(",")), (n = d.length);
+        if (n < 3 || n > 4) return null;
+        (x.r = i(r[3] == "a" ? r.slice(5) : r.slice(4))),
+          (x.g = i(g)),
+          (x.b = i(b)),
+          (x.a = a ? parseFloat(a) : -1);
+      } else {
+        if (n == 8 || n == 6 || n < 4) return null;
+        if (n < 6)
+          d =
+            "#" +
+            d[1] +
+            d[1] +
+            d[2] +
+            d[2] +
+            d[3] +
+            d[3] +
+            (n > 4 ? d[4] + d[4] : "");
+        d = i(d.slice(1), 16);
+        if (n == 9 || n == 5)
+          (x.r = (d >> 24) & 255),
+            (x.g = (d >> 16) & 255),
+            (x.b = (d >> 8) & 255),
+            (x.a = m((d & 255) / 0.255) / 1000);
+        else
+          (x.r = d >> 16), (x.g = (d >> 8) & 255), (x.b = d & 255), (x.a = -1);
+      }
+      return x;
+    };
+  (h = c0.length > 9),
+    (h = a ? (c1.length > 9 ? true : c1 == "c" ? !h : false) : h),
+    (f = this.pSBCr(c0)),
+    (P = p < 0),
+    (t =
+      c1 && c1 != "c"
+        ? this.pSBCr(c1)
+        : P
+        ? { r: 0, g: 0, b: 0, a: -1 }
+        : { r: 255, g: 255, b: 255, a: -1 }),
+    (p = P ? p * -1 : p),
+    (P = 1 - p);
+  if (!f || !t) return null;
+  if (l)
+    (r = m(P * f.r + p * t.r)),
+      (g = m(P * f.g + p * t.g)),
+      (b = m(P * f.b + p * t.b));
+  else
+    (r = m((P * f.r ** 2 + p * t.r ** 2) ** 0.5)),
+      (g = m((P * f.g ** 2 + p * t.g ** 2) ** 0.5)),
+      (b = m((P * f.b ** 2 + p * t.b ** 2) ** 0.5));
+  (a = f.a),
+    (t = t.a),
+    (f = a >= 0 || t >= 0),
+    (a = f ? (a < 0 ? t : t < 0 ? a : a * P + t * p) : 0);
+  if (h)
+    return (
+      "rgb" +
+      (f ? "a(" : "(") +
+      r +
+      "," +
+      g +
+      "," +
+      b +
+      (f ? "," + m(a * 1000) / 1000 : "") +
+      ")"
+    );
+  else
+    return (
+      "#" +
+      (4294967296 + r * 16777216 + g * 65536 + b * 256 + (f ? m(a * 255) : 0))
+        .toString(16)
+        .slice(1, f ? undefined : -2)
+    );
+};
