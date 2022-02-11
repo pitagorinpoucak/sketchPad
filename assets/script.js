a413f0de8b6be6fb14686991ab820a229063efe1
pitagorinpoucak <rookie.1.8@gmail.com>
2022-02-11 13:00:52 +0100

    Basic Etch-a-Sketch
    - Coded basic functionality, works as intended.
    - Decided to use dots instead of squares.

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
-const gridSize = prompt("How big should the grid be?");
+const gridSize = 10; //prompt("How big should the grid be?");
+let colorsArray = ["blue", "black", "yellow", "green", "red", "grey", "orange"];
 
 drawGrid();
 
 function drawGrid() {
   const gridContainer = document.getElementById("grid-container");
-  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 20px)`;
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 60px)`;
 
   for (let i = 0; i < gridSize * gridSize; i++) {
     gridContainer.appendChild(document.createElement("div"));
@@ -12,5 +13,12 @@ function drawGrid() {
   const gridItems = document.querySelectorAll("#grid-container div");
   gridItems.forEach((item) => {
     item.classList.add("grid-item");
+    item.addEventListener("mouseover", changeColor);
   });
+}
+
+function changeColor(e) {
+  const colour =
+    colorsArray[Math.floor(Math.random() * (colorsArray.length - 1))];
+  e.target.style.backgroundColor = colour;
 }
